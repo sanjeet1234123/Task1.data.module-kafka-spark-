6ae40d0f689da45d74257b92a7340408417380e8
sanjeet1234123 <sanjeetjha9073@gmail.com>
2023-10-21 03:17:58 +0530

    dd

--- analysis.py
+++ analysis.py
@@ -2,17 +2,16 @@ import os
 os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.2.4,org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.4,io.delta:delta-core_2.12:2.0.0 pyspark-shell'
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
-import pyspark
 from delta import *
 
-spark = pyspark.sql.SparkSession.builder.appName("deltaANA") \
+spark = SparkSession.builder.appName("deltaANA") \
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")\
     .getOrCreate()
 
 # Task 1: Read data from Delta Lake
 
-df = spark.read.format("delta").load("/home/xs391-sanjha/code/delta")
+df = spark.read.format("delta").load("/home/xs391-sanjha/Task1.data.module(kafka&spark)/delta")
 
 # Task 2: Calculate number of distinct signal_ts datapoints per day
 
--- readDelta.py
+++ readDelta.py
@@ -1,10 +1,9 @@
 import os
 os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.2.4,org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.4,io.delta:delta-core_2.12:2.0.0 pyspark-shell'
 from pyspark.sql import SparkSession
-import pyspark
-from delta import *
 
-spark = pyspark.sql.SparkSession.builder.appName("MyApp") \
+
+spark = SparkSession.builder.appName("MyApp") \
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")\
     .getOrCreate()
